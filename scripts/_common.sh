63b0d8e74e7481bebf9c994e90a288edc4a113e0
Robles Rodolphe <rodolphe.robles@sfr.fr>
2025-03-01 20:56:04 +0100

    Update _common.sh
    try fix variable

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,4 +4,4 @@
 # COMMON VARIABLES
 #=================================================
 
-IPv4_YUNOHOST_SERVER="$(curl -s https://ip.yunohost.org/)"
+IP_YUNOHOST_SERVER="$(curl -s https://ip.yunohost.org/)"
