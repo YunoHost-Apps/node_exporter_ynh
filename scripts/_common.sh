bed09606018b7e1965de68593b8af3ceb2d49b1a
rodinux <rodolphe.robles@sfr.fr>
2025-03-02 21:52:01 +0100

    try fix hook

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,3 +7,7 @@
 ip_yunohost_server="$(curl -s https://ip.yunohost.org/)"
 
 line_port=$(iptables -xvnL --line-numbers |grep $port |cut -d' ' -f1)
+
+ip_promotheus_server=$(ynh_app_setting_get --app=$app --key=ip_promotheus_server)
+
+
