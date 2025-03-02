5d12f91aa881a4380747e9be98f610ed1b8fb1de
rodinux <rodolphe.robles@sfr.fr>
2025-03-02 20:42:07 +0100

    variable number line

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,3 +5,5 @@
 #=================================================
 
 ip_yunohost_server="$(curl -s https://ip.yunohost.org/)"
+
+line_port=$(iptables -xvnL --line-numbers |grep $port |cut -d' ' -f1)
