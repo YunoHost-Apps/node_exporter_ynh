edafab7ba837d1e1244ac49cd2bc2d1cb727af68
rodinux <rodolphe.robles@sfr.fr>
2025-03-02 22:28:42 +0100

    try fix hook, error typo

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,8 +8,8 @@ ip_yunohost_server="$(curl -s https://ip.yunohost.org/)"
 
 line_port=$(iptables -xvnL --line-numbers |grep $port |cut -d' ' -f1)
 
-if [ -z $ip_promotheus_server ]; then
-	ip_promotheus_server=$(ynh_app_setting_get $app ip_promotheus_server)
+if [ -z $ip_prometheus_server ]; then
+	ip_prometheus_server=$(ynh_app_setting_get $app ip_prometheus_server)
 fi
 
 
