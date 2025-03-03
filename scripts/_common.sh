952bb2f7fba5ac23179aeec1f35a00c6b12ad2f2
rodinux <rodolphe.robles@sfr.fr>
2025-03-03 01:42:24 +0100

    add a rule in hook

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,10 +7,8 @@
 ip_yunohost_server="$(curl -s https://ip.yunohost.org/)"
 
 line_port=$(iptables -xvnL --line-numbers |grep $port |cut -d' ' -f1)
+line_port6=$(ip6tables -xvnL --line-numbers |grep $port |cut -d' ' -f1)
 
 if [ -z $ip_prometheus_server ]; then
 	ip_prometheus_server=$(ynh_app_setting_get $app ip_prometheus_server)
 fi
-
-
-
