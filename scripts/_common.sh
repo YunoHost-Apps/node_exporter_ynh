267f2690932b9f0f7a7378bc949c0c9099e6ab50
Rodolphe Robles <rodinux07@rodinux.fr>
2025-04-21 21:57:26 +0200

    try fix set value settings ips

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,11 +8,4 @@
 line_port=$(iptables -xvnL --line-numbers |grep $port |cut -d' ' -f1) || true
 line_port6=$(ip6tables -xvnL --line-numbers |grep $port |cut -d' ' -f1) || true
 
-
-if [ "$external" == "0" ]; then
-	ip_yunohost_server="127.0.0.1"
-fi
-
-if [ "$external" == "1" ]; then
-	ip_yunohost_server="$(curl -s https://ip.yunohost.org/)"
-fi
+ip_yunohost_server="$(ynh_app_setting_get --app="$app" --key="ip_yunohost_server")"
