959f9ae582d7e976b97b8cfa29a97a6714b7afb4
Rodolphe Robles <rodinux07@rodinux.fr>
2025-04-21 21:53:27 +0200

    try fix set value settings ips

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@ line_port=$(iptables -xvnL --line-numbers |grep $port |cut -d' ' -f1) || true
 line_port6=$(ip6tables -xvnL --line-numbers |grep $port |cut -d' ' -f1) || true
 
 
-if [ "$external" == "0" ]]; then
+if [ "$external" == "0" ]; then
 	ip_yunohost_server="127.0.0.1"
 fi
 
