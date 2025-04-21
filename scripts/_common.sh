1c9c37efd5514365c7f0402afa119ddab6ae7c09
Rodolphe Robles <rodinux07@rodinux.fr>
2025-04-21 22:59:01 +0200

    try fix set value settings ips

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,12 +8,12 @@
 line_port=$(iptables -xvnL --line-numbers |grep $port |cut -d' ' -f1) || true
 line_port6=$(ip6tables -xvnL --line-numbers |grep $port |cut -d' ' -f1) || true
 
-# if [ -z $ip_prometheus_server ]; then
-# 	ip_prometheus_server=$(ynh_app_setting_get "$app" ip_prometheus_server)
-# fi
+if [ -z $ip_prometheus_server ]; then
+	ip_prometheus_server=$(ynh_app_setting_get "$app" ip_prometheus_server)
+fi
 
-# if [ "$ip_prometheus_server" == "127.0.0.1" ]; then
-# 	ip_yunohost_server="127.0.0.1"
-# else
-# 	ip_yunohost_server="$(curl -s https://ip.yunohost.org/)"
-# fi
+if [ "$ip_prometheus_server" == "127.0.0.1" ]; then
+	ip_yunohost_server="127.0.0.1"
+else
+	ip_yunohost_server="$(curl -s https://ip.yunohost.org/)"
+fi
