bc71de1216b6f8c3ce4b42d2c2af2b3bf7b94414
Rodolphe Robles <rodinux07@rodinux.fr>
2025-04-21 22:53:36 +0200

    try fix set value settings ips

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,8 +5,8 @@
 #=================================================
 
 
-# line_port=$(iptables -xvnL --line-numbers |grep $port |cut -d' ' -f1) || true
-# line_port6=$(ip6tables -xvnL --line-numbers |grep $port |cut -d' ' -f1) || true
+line_port=$(iptables -xvnL --line-numbers |grep $port |cut -d' ' -f1) || true
+line_port6=$(ip6tables -xvnL --line-numbers |grep $port |cut -d' ' -f1) || true
 
 # if [ -z $ip_prometheus_server ]; then
 # 	ip_prometheus_server=$(ynh_app_setting_get "$app" ip_prometheus_server)
