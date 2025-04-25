86c02755b592d4de8cceb0e61ee405185b06d887
Rodolphe Robles <rodinux07@rodinux.fr>
2025-04-25 18:22:18 +0200

    needed for upgrade

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,8 +8,10 @@
 line_port=$(iptables -xvnL --line-numbers |grep $port |cut -d' ' -f1) || true
 line_port6=$(ip6tables -xvnL --line-numbers |grep $port |cut -d' ' -f1) || true
 
-if [ -z $ip_prometheus_server ]; then
+if [ -z "$ip_prometheus_server" ]; then
 	ip_prometheus_server=$(ynh_app_setting_get "$app" ip_prometheus_server)
+else
+	ip_prometheus_server="127.0.0.1"
 fi
 
 if [ "$ip_prometheus_server" == "127.0.0.1" ]; then
