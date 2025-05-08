556ce5063a60f131830d52076cbf4645f3f77cfc
Rodolphe Robles <rodinux07@rodinux.fr>
2025-05-08 23:41:02 +0200

    seems now only the rule choose by the variable ip_prometheus_server keep a port open

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,5 +5,5 @@
 #=================================================
 
 line_port=$(iptables -xvnL --line-numbers |grep $port |cut -d' ' -f1) || true
-line_port6=$(ip6tables -xvnL --line-numbers |grep $port |cut -d' ' -f1) || true
-line_port_exclude=$(iptables -xvnL --line-numbers | grep '^[0-9]\+\s\+[0-9]\+\s\+[0-9]\+\s\+ACCEPT\s\+[0-9]\+\s\+--\s\+\*\s\+\*\s\+0\.0\.0\.0/0\s\+0\.0\.0\.0/0.*dpt:$port' | awk '{print $1}')
+#line_port6=$(ip6tables -xvnL --line-numbers |grep $port |cut -d' ' -f1) || true
+#line_port_exclude=$(iptables -xvnL --line-numbers | grep '^[0-9]\+\s\+[0-9]\+\s\+[0-9]\+\s\+ACCEPT\s\+[0-9]\+\s\+--\s\+\*\s\+\*\s\+0\.0\.0\.0/0\s\+0\.0\.0\.0/0.*dpt:$port' | awk '{print $1}')
