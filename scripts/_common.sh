209a36d11d4ce3ed3be34feb0b3b49f5463914ba
Rodolphe Robles <rodinux07@rodinux.fr>
2025-05-08 23:13:18 +0200

    try fix tags for IPs

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,6 +4,6 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-
 line_port=$(iptables -xvnL --line-numbers |grep $port |cut -d' ' -f1) || true
 line_port6=$(ip6tables -xvnL --line-numbers |grep $port |cut -d' ' -f1) || true
+line_port_exclude=$(iptables -xvnL --line-numbers | grep '^[0-9]\+\s\+[0-9]\+\s\+[0-9]\+\s\+ACCEPT\s\+[0-9]\+\s\+--\s\+\*\s\+\*\s\+0\.0\.0\.0/0\s\+0\.0\.0\.0/0.*dpt:$port' | awk '{print $1}')
