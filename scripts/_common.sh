8aabd280007d4c226f378ba16bdc365c046443a9
Robles Rodolphe <rodolphe.robles@sfr.fr>
2025-06-06 09:48:45 +0200

    Update scripts/_common.sh
    yep
    
    Co-authored-by: Alexandre Aubin <4533074+alexAubin@users.noreply.github.com>

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,6 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-ip_yunohost_server=$(curl -s https://ip.yunohost.org/)
 
 line_port=$(iptables -xvnL --line-numbers |grep $port |cut -d' ' -f1) || true
 
