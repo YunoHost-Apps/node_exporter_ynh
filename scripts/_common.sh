44a876aa7966208d466cba63f5e14ac79b844355
Rodolphe Robles <rodinux07@rodinux.fr>
2025-06-15 23:23:07 +0200

    try fix upgrade

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,9 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-if [ "$external" == true ]; then
+if [ "$external" -eq "1" ]; then
 	bind_address="127.0.0.1"
-elif [ "$external" == false ]; then
+elif [ "$external" -eq "0" ]; then
 	bind_address="0.0.0.0"
 fi
 
