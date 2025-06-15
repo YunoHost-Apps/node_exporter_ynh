2d6494f66416612c457fc1c0f2cb5f3669adb967
Rodolphe Robles <rodinux07@rodinux.fr>
2025-06-16 00:51:49 +0200

    try fix bind_adde

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,9 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-if [ "$external" == false ]; then
+if [[ "$external" == false ]]; then
 	bind_address="127.0.0.1"
-elif [ "$external" == true ]; then
+elif [[ "$external" == true ]]; then
 	bind_address="0.0.0.0"
 fi
 
