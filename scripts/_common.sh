1b119f330d3c2f1d7e6408670e4d6c94ed966841
Alexandre Aubin <4533074+alexAubin@users.noreply.github.com>
2025-11-02 19:07:16 +0100

    Update _common.sh: no need to define the bind_address anymoar

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-#=================================================
-# COMMON VARIABLES AND CUSTOM HELPERS
-#=================================================
-
-if [[ "external" == false ]]; then
-	bind_address="127.0.0.1"
-elif [[ "external" == true ]]; then
-	bind_address="0.0.0.0"
-fi
 
